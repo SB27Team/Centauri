fc6d35d306e8605a9d276a5717e285517e64b3f1
superblaubeere27 <arschwixxa123@gmail.com>
2018-05-17 17:46:16 +0200

    Add JAR Loading function

--- src/main/java/net/sb27team/centauri/Centauri.java
+++ src/main/java/net/sb27team/centauri/Centauri.java
@@ -1,12 +1,24 @@
 package net.sb27team.centauri;
 
+import net.sb27team.centauri.controller.MainMenuController;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Objects;
 import java.util.logging.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class Centauri {
     public static Centauri INSTANCE = new Centauri();
     public static final Logger LOGGER = Logger.getAnonymousLogger();
     public static boolean DEBUG = true;
+    private ZipFile openedZipFile = null;
+    private File openedFile = null;
+    private List<ZipEntry> loadedZipEntries = new ArrayList<>();
 
     static {
         ConsoleHandler handler = new ConsoleHandler();
@@ -15,19 +27,81 @@ public class Centauri {
         handler.setLevel(DEBUG ? Level.FINEST : Level.INFO);
         LOGGER.addHandler(handler);
         LOGGER.setUseParentHandlers(false);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> INSTANCE.shutDown()));
 //        LOGGER.setFilter(record -> DEBUG || record.getLevel().intValue() >= Level.INFO.intValue());
     }
 
     public void shutDown() {
         LOGGER.info("Shutting down...");
-        System.exit(0);
+        closeFile();
+//        System.exit(0);
     }
 
     public void closeFile() {
         LOGGER.fine("Closing file...");
+
+        if (openedZipFile != null) {
+            try {
+                openedZipFile.close();
+            } catch (IOException e) {
+                report(e);
+            }
+        }
+
+        openedZipFile = null;
+        openedFile = null;
+
+        if (loadedZipEntries != null) {
+            loadedZipEntries.clear();
+        }
+        MainMenuController.INSTANCE.updateTree();
     }
 
     public void openFile(File file) {
+        Objects.requireNonNull(file, "File is null");
+
         LOGGER.info("Opening " + file.getName());
+
+        if (openedFile != null) {
+            closeFile();
+        }
+
+        MainMenuController.INSTANCE.setStatus("Opening " + file.getAbsolutePath());
+
+        try {
+            openedZipFile = new ZipFile(file);
+            openedFile = file;
+            Enumeration<? extends ZipEntry> entries = openedZipFile.entries();
+
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+
+                this.loadedZipEntries.add(entry);
+            }
+
+            MainMenuController.INSTANCE.updateTree();
+            MainMenuController.INSTANCE.setStatus("Ready");
+        } catch (Exception e) {
+            closeFile();
+            MainMenuController.INSTANCE.updateTree();
+            MainMenuController.INSTANCE.setStatus("Error " + e);
+            report(e);
+        }
+    }
+
+    private void report(Exception e) {
+        LOGGER.severe("ERROR: " + e);
+
+        if (DEBUG) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<ZipEntry> getLoadedZipEntries() {
+        return loadedZipEntries;
+    }
+
+    public File getOpenedFile() {
+        return openedFile;
     }
 }
--- src/main/java/net/sb27team/centauri/controller/MainMenuController.java
+++ src/main/java/net/sb27team/centauri/controller/MainMenuController.java
@@ -2,22 +2,42 @@ package net.sb27team.centauri.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import net.sb27team.centauri.Centauri;
 import net.sb27team.centauri.controller.utils.Utils;
 
+import java.io.File;
+import java.util.*;
+import java.util.zip.ZipEntry;
+
 public class MainMenuController {
+    private static Image JAVA_ICON;
+    private static Image FOLDER_ICON;
+    public static MainMenuController INSTANCE = new MainMenuController();
 
     @FXML
-    private TreeView treeView;
+    private TreeView<String> resourceTree;
+    @FXML
+    private Label rightStatus;
+    @FXML
+    private Label leftStatus;
 
     public void initialize() {
+        INSTANCE = this;
+
+        JAVA_ICON = new Image(getClass().getResourceAsStream("/icons/java.png"));
+        FOLDER_ICON = new Image(getClass().getResourceAsStream("/icons/folder.png"));
 
     }
 
     @FXML
     public void quitMenuItemClicked(ActionEvent e) {
-        Centauri.INSTANCE.shutDown();
+//        Centauri.INSTANCE.shutDown();
+        System.exit(0);
     }
 
     @FXML
@@ -32,6 +52,69 @@ public class MainMenuController {
         } catch (Exception e1) {
             e1.printStackTrace();
         }
+    }
+
+    public void setStatus(String text) {
+        leftStatus.setText(text);
+    }
+
+    public void updateTree() {
+        if (Centauri.INSTANCE.getOpenedFile() == null) {
+            resourceTree.setRoot(null);
+            return;
+        }
+
+        HashMap<String, List<ZipEntry>> packageEntryMap = new HashMap<>();
+
+        for (ZipEntry zipEntry : Centauri.INSTANCE.getLoadedZipEntries()) {
+            if (zipEntry.isDirectory()) {
+                continue;
+            }
+            String parent = new File(zipEntry.getName()).getParent();
+
+            if (!packageEntryMap.containsKey(parent)) {
+                packageEntryMap.put(parent, new ArrayList<>());
+            }
+            packageEntryMap.get(parent).add(zipEntry);
+        }
+
+        TreeItem<String> rootItem = new TreeItem<>(Centauri.INSTANCE.getOpenedFile().getName(), new ImageView(FOLDER_ICON));
+
+//        for (Map.Entry<String, List<ZipEntry>> stringListEntry : packageEntryMap.entrySet()) {
+//
+//
+//        }
+        packageEntryMap.entrySet().stream().sorted((o1, o2) -> {
+            String obj1 = o1.getKey();
+            String obj2 = o2.getKey();
+
+            if (obj1 == null) {
+                return 1;
+            }
+            if (obj2 == null) {
+                return -1;
+            }
+            if (obj1.equals( obj2 )) {
+                return 0;
+            }
+            return obj1.compareTo(obj2);
+        }).forEach(entry -> {
+            if (entry.getKey() == null) {
+                for (ZipEntry zipEntry : entry.getValue()) {
+                    rootItem.getChildren().add(new TreeItem<>(new File(zipEntry.getName()).getName(), new ImageView(JAVA_ICON)));
+                }
+            } else {
+                TreeItem<String> pack = new TreeItem<>(entry.getKey().replace('\\', '.').replace('/', '.'), new ImageView(FOLDER_ICON));
+
+                for (ZipEntry zipEntry : entry.getValue()) {
+                    pack.getChildren().add(new TreeItem<>(new File(zipEntry.getName()).getName(), new ImageView(JAVA_ICON)));
+                }
+
+                rootItem.getChildren().add(pack);
+            }
+        });
+
+        resourceTree.setRoot(rootItem);
     }
 
 }

248fb2c4e0dcdce9509996a805b3ed739c874965
superblaubeere27 <arschwixxa123@gmail.com>
2018-05-20 09:02:59 +0200

    Fixed FileExplorer

--- src/main/java/net/sb27team/centauri/explorer/FileExplorer.java
+++ src/main/java/net/sb27team/centauri/explorer/FileExplorer.java
@@ -1,11 +1,16 @@
+/*
+ * Copyright (c) 2018 SinC (superblaubeere27, Cubixy, Cython, SplotyCode, Xc3pt1on)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package net.sb27team.centauri.explorer;
 
 import com.google.common.collect.Lists;
-import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
-import javafx.scene.image.ImageView;
-import net.sb27team.centauri.resource.ResourceManager;
 import org.apache.commons.io.FilenameUtils;
 
 import java.util.ArrayList;
@@ -28,7 +33,7 @@ public class FileExplorer {
             String file = zipEntry.getName();
 
             String name = file.substring(file.lastIndexOf(separator)+1);
-            String[] path = file.substring(0, file.lastIndexOf(separator)).split(separator);
+            String[] path = file.lastIndexOf(separator) != -1 ? file.substring(0, file.lastIndexOf(separator)).split(separator) : new String[0];
 
             //System.out.println("name=" + name + " path=" + file.substring(0, file.lastIndexOf(separator)));
 

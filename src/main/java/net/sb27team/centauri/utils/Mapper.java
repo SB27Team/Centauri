ef819086ca7959011d9984c30860b0be4c299767
david <davidscandurra@gmail.com>
2018-05-21 13:20:14 +0200

    Added Alerts for Mapping

--- src/main/java/net/sb27team/centauri/utils/Mapper.java
+++ src/main/java/net/sb27team/centauri/utils/Mapper.java
@@ -1,5 +1,8 @@
 package net.sb27team.centauri.utils;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +24,7 @@ public class Mapper {
     public void addFile(File file) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
             String line;
+            int skip = 0;
             while ((line = reader.readLine()) != null) {
                 String[] split = new String[0];
                 for (String seperator : SEPERATORS) {
@@ -28,9 +32,14 @@ public class Mapper {
                     split = line.split(seperator);
                 }
                 if(split.length == 2) map.put(split[0], split[1]);
+                else skip++;
             }
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Mapped " + map.size() + " values!\nSkipped: " + skip + " lines!", ButtonType.OK);
+            alert.setTitle("Mapping Complete");
+            alert.setHeaderText("Mapping Complete");
+            alert.show();
         } catch (IOException ex) {
-            ex.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed reading File!", ButtonType.CLOSE).show();
         }
     }
 
